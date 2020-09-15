f6e90345fea47491ebc60ee9dfd6540a4ffa0a18
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-16 01:52:23 +0300

    Styles for the header block and burger menu

---
+++ src/styles/Header.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+
+export const Header = styled.header`
+    height: 15vh;
+    background: #5b78c7;
+` 
+
+export const HeaderNav = styled.nav`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    height: 100%;
+` 
+
+export const NavHumburger = styled.div`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        position: absolute;
+        display: block;
+        cursor: pointer;
+        right: 5%;
+        top: 7%;
+        transform: translate(-5%, -50%);
+        z-index: 2;
+    }
+`
+
+export const Line = styled.div`
+    @media screen and (max-width: 768px) {
+        width: 30px;
+        height: 3px;
+        background: white;
+        margin: 5px;
+    }
+`
+
+export const NavWrapper = styled.ul`
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+
+    @media screen and (max-width: 768px) {
+        position: fixed;
+        height: 60vh;
+        width: 100%;
+        align-items: center;
+        flex-direction: column;
+        background: #5b78c7;
+        clip-path: circle(10px at 90% -10%);
+        transition: all 1s ease-out;
+        pointer-events: none;
+    }
+
+    &.open {
+        clip-path: circle(1000px at 90% -10%);
+        pointer-events: all;
+    }
+`
+
+export const NavLogo = styled.h1`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: flex;
+        font-family: 'Architects Daughter';
+        align-items: center;
+        font-size: 2em;
+        letter-spacing: 5px;
+        user-select: none;
+    }
+`
+
+export const NavItem = styled.li`
+    display: flex;
+    height: 100%;
+    align-items: center;
+    width: 8.4em;
+
+    & a {
+        padding: 0px 15px;
+        width: 100%;
+        height: 70%;
+        display: flex;
+        justify-content: space-around;
+        border-radius: 30px;
+        align-items: center;
+        text-decoration: none;
+        transition: all 0.4s ease;
+
+        &:hover {
+            background: #203748;
+            transform: scale(0.95);
+        }
+    }
+` 
