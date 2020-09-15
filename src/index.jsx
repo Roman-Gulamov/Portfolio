72fab40d48853583ffab268c13759d0b96fc006b
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-16 01:49:33 +0300

    Added styles for fonts

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './Components/App';
 
+import './assets/fonts/kufam/stylesheet.css';
+import './assets/fonts/architectsDaughter/stylesheet.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
