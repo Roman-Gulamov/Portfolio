ece4b0d0ce5b3e71700f470ec410a853305ba76a
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-16 23:42:45 +0300

    Footer block

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { Container } from '../../styles/Container';
+import {
+    FooterWrapper,
+    FooterSocial,
+    SocialItem
+} from '../../styles/Footer';
+
+import { SOCIAL_LINKS } from './footerData';
+
 
 export const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <FooterWrapper>
+            <Container>
+                <FooterSocial>
+                    {SOCIAL_LINKS.map(({ name, path, icon }) =>
+                        <SocialItem href={path} target='_blank' key={name}>
+                            <FontAwesomeIcon icon={icon} size="2x" />
+                        </SocialItem>
+                    )}
+                </FooterSocial>
+            </Container>
+        </FooterWrapper>
     )
 }
