7addae3d41f78d88b58b3a40a77b8032b1981143
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-16 23:41:08 +0300

    Footer styles

---
+++ src/styles/Footer.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+
+export const FooterWrapper = styled.footer`
+    position: fixed;
+    bottom: 0;
+    height: 10vh;
+    width: 100%;
+    background: #355267;
+    z-index: 9999;
+` 
+
+export const FooterSocial = styled.div`
+    height: 10vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+` 
+
+export const SocialItem = styled.a`
+    & svg path {
+        transition: all 0.4s ease;
+    }
+    
+    &:hover svg path {
+        transform: scale(0.92);
+    }
+` 
