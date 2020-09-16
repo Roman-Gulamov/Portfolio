2b4e060b5ec09e1ad4da5c5b46849e02c1fc29e4
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-16 23:43:52 +0300

    Future resume page

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Container } from '../../styles/Container';
+import {
+    ResumeWrapper,
+} from '../../styles/Resume';
+
+
 export const Resume = () => {
     return (
-        <div>
-            Resume
-        </div>
+        <ResumeWrapper>
+        </ResumeWrapper>
     )
 }
