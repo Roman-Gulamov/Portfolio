c4361e9106f0477d7bc5fddb31159f18311d047d
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-17 01:55:40 +0300

    Style File for Form Feedback

---
+++ src/styles/Feedback.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+
+export const FeedbackWrapper = styled.div`
+    width: 100%; 
+    margin: 0 auto;  
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    text-align: center;
+    z-index: 1050;
+` 
+
+export const FeedbackForm = styled.div`
+
+` 
+
+export const FeedbackClose = styled.div`
+
+` 
