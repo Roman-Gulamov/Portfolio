544451ece4572ed10755ef8bb29c5b6e4713f9f8
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-16 23:44:22 +0300

    File for social networks map

---
+++ src/Components/Footer/footerData.js
@@ -0,0 +1,25 @@
+import { faVk, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
+export const SOCIAL_LINKS = [
+    {
+        name: 'vk',
+        path: 'https://vk.com/memento_turk',
+        icon: faVk,
+    },
+    {
+        name: 'instagram',
+        path: 'https://www.instagram.com/memento_turk',
+        icon: faInstagram,
+    },
+    {
+        name: 'gitHub',
+        path: 'https://github.com/Roman-Gulamov',
+        icon: faGithub,
+    },
+    {
+        name: 'linkedIn',
+        path: 'https://www.linkedin.com/in/роман-гуламов-5712b31b2/',
+        icon: faLinkedin,
+    }
+]
