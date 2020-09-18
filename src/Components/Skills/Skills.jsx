00bb5a1403b7694603a1a3e5c46e7cd3efd485b9
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-19 00:34:49 +0300

    Skill component

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import { Container } from '../../styles/Container';
+import { SkillsWrapper } from '../../styles/Skills';
+import { SkillsMap } from './SkillsMap';
+
 
 export const Skills = () => {
     return (
@@ -11,7 +15,11 @@ export const Skills = () => {
             <meta name="description" content="Skills" />
             <link rel="canonical" href="https://roman-gulamov.github.io/Portfolio/#/skills" />
         </Helmet>
-            Skills
+        <Container>
+            <SkillsWrapper>
+                <SkillsMap />
+            </SkillsWrapper>
+        </Container>
         </>
     )
 }
