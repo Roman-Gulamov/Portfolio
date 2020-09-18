02df499af279507613a1a73e3b4cdace996fdfdd
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-19 00:36:30 +0300

    File for mapping and building the main markup

---
+++ src/Components/Skills/SkillsMap.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { SKILLS_DATA } from './SkillsData';
+import {
+    SkillsItem,
+    SkillsImg,
+    ImgLanguage,
+    SkillsProgress,
+    ProgressInformation,
+    ProgressTitle,
+    ProgressProcent,
+    ProgressBar
+} from '../../styles/Skills';
+
+
+export const SkillsMap = () => {
+    return (
+        <>
+        {SKILLS_DATA.map(({ name, src, procent, background }) =>
+            <SkillsItem key={name}>
+                <SkillsImg>
+                    <ImgLanguage src={src} alt={name}/>
+                </SkillsImg>
+                <SkillsProgress>
+                    <ProgressInformation>
+                        <ProgressTitle>{name}</ProgressTitle>
+                        <ProgressProcent>{procent}</ProgressProcent>
+                    </ProgressInformation>
+                    <ProgressBar 
+                        procent={procent} 
+                        background={background} 
+                    />
+                </SkillsProgress>
+            </SkillsItem>
+        )}
+        </>
+    )
+}
