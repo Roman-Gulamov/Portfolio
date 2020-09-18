0450c42e39b349f45ad320cef307cf6a9ea9aa27
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-18 04:56:50 +0300

    Contact block styles

--- src/styles/Feedback.js
+++ src/styles/Feedback.js
@@ -2,21 +2,53 @@ import styled from 'styled-components';
 
 
 export const FeedbackWrapper = styled.div`
-    width: 100%; 
-    margin: 0 auto;  
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    text-align: center;
-    z-index: 1050;
+    margin: 1.2em 0em 15vh;
 ` 
 
 export const FeedbackForm = styled.div`
+    width: 40%;
+    margin: 0 auto;
 
+    @media screen and (max-width: 1000px) {
+        width: 80%;
+    }
 ` 
 
-export const FeedbackClose = styled.div`
+export const FormItem = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-bottom: 1.3em;
 
+    & textarea {
+        resize: none;
+        height: 5.3em;
+    }
+
+    & input, & textarea {
+        padding: 0.9em;
+        font-size: 0.7em;
+        border-radius: 2em;
+        border: none;
+        background-color: #E9EFF6;
+        color: #000000c9;
+
+        &:focus {
+            box-shadow: 0 0 2pt 2pt #fc6c18;
+        }
+    }
+
+    & span {
+        position: absolute;
+        top: 106%;
+        left: 4%;
+        color: #fc6c18;
+        font-size: 0.7em;
+    }
+` 
+
+export const FormLabel = styled.label`
+    text-align: center;
+    line-height: 1.6;
 ` 
