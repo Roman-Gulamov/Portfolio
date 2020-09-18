2e29fd8831fb1883ededb448d6f14097869207fd
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-18 04:59:47 +0300

    Array with data for future fields

---
+++ src/Components/Feedback/FormMap/InputData.js
@@ -0,0 +1,26 @@
+export const INPUT_DATA = [
+    {
+        name: 'username',
+        component: 'input',
+        autoFocus: true,
+        autoComplete: 'true',
+        type: 'text',
+        title: 'Name'
+    },
+    {
+        name: 'email',
+        component: 'input',
+        autoFocus: false,
+        autoComplete: 'true',
+        type: 'email',
+        title: 'Email'
+    },
+    {
+        name: 'message',
+        component: 'textarea',
+        autoFocus: false,
+        autoComplete: null,
+        type: null,
+        title: 'Message'
+    },
+]
