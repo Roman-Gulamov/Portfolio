7c17217d8f665fd5362b15b2eb26606b162c5e66
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-18 04:58:07 +0300

    Form submit function

---
+++ src/Components/Feedback/submitForm.js
@@ -0,0 +1,18 @@
+import { createBrowserHistory } from "history";
+import Axios from "axios";
+
+
+export const submitForm = (values) => {
+    const history = createBrowserHistory();
+    const axios = Axios.create();
+    
+    axios.post("https://formspree.io/xbjpredk", {...values})
+    .then(response => {
+        if (response.status === 200) {
+            history.push('/train-search/#/profile');
+            history.go(0);
+
+            window.location.reload(); // for mobile phone
+        }
+    })
+}
