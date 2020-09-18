706e846d4638cfe898a24b841857939ae2aa56f9
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-19 00:34:35 +0300

    Skill data file

---
+++ src/Components/Skills/SkillsData.js
@@ -0,0 +1,74 @@
+import ReactImg from '../../assets/images/ReactImg.png';
+import JavaScript from '../../assets/images/JavaScript.png';
+import HTML from '../../assets/images/HTML.png';
+import SCSS from '../../assets/images/SCSS.png';
+import jQuery from '../../assets/images/jQuery.png';
+import StyledComponents from '../../assets/images/styled-components.png';
+import Webpack from '../../assets/images/Webpack.png';
+import Gulp from '../../assets/images/Gulp.png';
+import TypeScript from '../../assets/images/TypeScript.png';
+import PHP from '../../assets/images/PHP.png';
+
+
+export const SKILLS_DATA = [
+    {
+        name: 'HTML5',
+        src: HTML,
+        procent: '99%',
+        background: '#D1382B'
+    },
+    {
+        name: 'SCSS',
+        src: SCSS,
+        procent: '95%',
+        background: '#CF649A'
+    },
+    {
+        name: 'jQuery',
+        src: jQuery,
+        procent: '90%',
+        background: '#0867AB'
+    },
+    {
+        name: 'React',
+        src: ReactImg,
+        procent: '80%',
+        background: '#56C2E0'
+    },
+    {
+        name: 'JavaScript',
+        src: JavaScript,
+        procent: '80%',
+        background: '#FFCA28'
+    },
+    {
+        name: 'Styled-components',
+        src: StyledComponents,
+        procent: '60%',
+        background: '#FBA2D9'
+    },
+    {
+        name: 'Webpack',
+        src: Webpack,
+        procent: '55%',
+        background: '#0D3B84'
+    },
+    {
+        name: 'Gulp',
+        src: Gulp,
+        procent: '50%',
+        background: '#D44B48'
+    },
+    {
+        name: 'TypeScript',
+        src: TypeScript,
+        procent: '35%',
+        background: '#295FBE'
+    },
+    {
+        name: 'PHP',
+        src: PHP,
+        procent: '10%',
+        background: '#8A94BF'
+    },
+]
