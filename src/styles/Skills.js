ae986aa0f525543ab3270fdf6526155f9fed5827
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-19 00:35:16 +0300

    Styles for the Skills component

---
+++ src/styles/Skills.js
@@ -0,0 +1,94 @@
+import styled, { keyframes } from 'styled-components';
+
+const progress = (props) => { 
+    return keyframes`
+        from {
+            width: 10%;
+        }
+        to {
+            width: ${props.procent || '0%'};
+        }
+    `
+}
+
+export const SkillsWrapper = styled.main`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 0.8em 0 15vh;
+` 
+
+export const SkillsItem = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    margin: 0.15em 0;
+
+    @media screen and (max-width: 761px) {
+        margin: 0.35em 0;
+    }
+` 
+
+export const SkillsImg = styled.div`
+    width: 16%;
+    text-align: end;
+` 
+
+export const ImgLanguage = styled.img`
+    width: 50%;
+    height: auto;
+
+    @media screen and (max-width: 761px) {
+        width: 100%; 
+    }
+`
+
+export const SkillsProgress = styled.div`
+    width: 70%;
+` 
+
+export const ProgressInformation = styled.div`
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5em;
+    
+    @media screen and (max-width: 500px) {
+        width: 100%;
+    }
+` 
+
+export const ProgressTitle = styled.h2`
+    @media screen and (max-width: 500px) {
+        font-size: 0.7em; 
+    }
+` 
+
+export const ProgressProcent = styled.div`
+    font-size: 0.8em;
+` 
+
+export const ProgressBar = styled.div`
+    width: 80%;
+    height: 0.55em;
+    position: relative;
+    border: none;
+    border-radius: 3em;
+    background-color: #fff;
+    
+    @media screen and (max-width: 500px) {
+        width: 100%;
+    }
+
+    &::after {
+        content: "";
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0%;
+        background: ${props => props.background || 'yellow'};
+        border-radius: 3em;
+        animation: ${progress} 2s ease-in 1 forwards;
+    }
+` 
