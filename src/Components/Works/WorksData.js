a8910f6daa114b139948f271cb0e17b3ca9ffcc1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-21 01:35:58 +0300

    Added test version of the data

---
+++ src/Components/Works/WorksData.js
@@ -0,0 +1,44 @@
+import TrainsAggregator from '../../assets/images/works/TrainsAggregator.png';
+import Subjects from '../../assets/images/works/Subjects.png';
+import Rapture from '../../assets/images/works/Rapture.png';
+import TicTac from '../../assets/images/works/TicTac.png';
+import TypeScriptForm from '../../assets/images/works/TypeScriptForm.png';
+
+
+export const WORKS_DATA = [
+    {
+        name: 'Trains-Aggregator',
+        img: TrainsAggregator,
+        git: 'https://github.com/Roman-Gulamov/train-search',
+        ghPages: 'https://roman-gulamov.github.io/train-search',
+        language: 'React'
+    },
+    {
+        name: 'Subjects',
+        img: Subjects,
+        git: 'https://github.com/Roman-Gulamov/Subjects',
+        ghPages: 'https://roman-gulamov.github.io/Subjects',
+        language: 'React'
+    },
+    {
+        name: 'Rapture',
+        img: Rapture,
+        git: 'https://github.com/Roman-Gulamov/Rapture',
+        ghPages: null,
+        language: 'jQuery'
+    },
+    {
+        name: 'Feedback-Form',
+        img: TypeScriptForm,
+        git: 'https://github.com/Roman-Gulamov/axiosform',
+        ghPages: null,
+        language: 'TypeScript'
+    },
+    {
+        name: 'Tic-Tac-Toe game',
+        img: TicTac,
+        git: 'https://github.com/Roman-Gulamov/Tic-tac-toe',
+        ghPages: 'https://roman-gulamov.github.io/tic-tac-toe',
+        language: 'React'
+    }
+]
