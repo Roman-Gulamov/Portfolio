a46e9ffad70ad8e8cf6951e92b9c8f42a1f03daf
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-23 04:15:25 +0300

    Removed unnecessary style

--- src/styles/Profile.js
+++ src/styles/Profile.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 
 export const ProfileWrapper = styled.main`
-    height: 100%;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
