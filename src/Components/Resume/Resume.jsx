7fa893986eaeb839ec0d932691e3eb8ff713e05a
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-23 04:16:17 +0300

    Added Resume component

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-// import { Container } from '../../styles/Container';
+import { Container } from '../../styles/Container';
+import { Button } from '../../styles/Button';
 import {
     ResumeWrapper,
+    ResumeTitle,
+    ResumeDescription,
+    ResumeDownload,
+    DownloadButton
 } from '../../styles/Resume';
 
 
@@ -16,8 +21,21 @@ export const Resume = () => {
             <meta name="description" content="Resume" />
             <link rel="canonical" href="https://roman-gulamov.github.io/Portfolio/#/resume" />
         </Helmet>
-        <ResumeWrapper>
-        </ResumeWrapper>
+        <Container>
+            <ResumeWrapper>
+                <ResumeTitle>My resume</ResumeTitle>
+                <ResumeDescription>
+                    You can see my resume for your references, and I hope {'\n'}
+                    that we can meet very soon 
+                    <span role="img" aria-label="handshake">&#129309;</span>
+                </ResumeDescription>
+                <ResumeDownload>
+                    <DownloadButton href="https://yadi.sk/i/U48lJNir0w3qJg" target="_blank">
+                        <Button>Show Resume</Button>
+                    </DownloadButton>
+                </ResumeDownload>
+            </ResumeWrapper>
+        </Container>
         </>
     )
 }
