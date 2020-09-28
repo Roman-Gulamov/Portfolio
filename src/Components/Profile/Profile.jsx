9fc2e8fa7a5fbf5dbfbc3c81f5f3077503830900
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-29 02:39:09 +0300

    Markup fix and tag replacement

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { 
-    HashRouter as Router,
-    NavLink 
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { Container } from '../../styles/Container';
 import { Button } from '../../styles/Button';
@@ -29,33 +26,31 @@ export const Profile = () => {
             <meta name="description" content="Profile" />
             <link rel="canonical" href="https://roman-gulamov.github.io/Portfolio/#/profile" />
         </Helmet>
-        <Router>
-            <Container>
-                <ProfileWrapper>
-                    <ProfileImage>
-                        <ImagePerson src={Roman} alt="Roman" />
-                    </ProfileImage>
-                    <ProfileInformation>
-                        <InformationName>
-                            Roman Gulamov
-                            <InformationPosition>
-                                Front-end Developer
-                            </InformationPosition>
-                        </InformationName>
-                        <InformationAbout>
-                            <span role="img" aria-label="give-five">Hello! &#128588;</span> {'\n'}
-                            <span role="img" aria-label="give-five">
-                                I am Roman Gulamov, Front-end Developer from Saint Petersburg &#127969;
-                            </span> {'\n'}  
-                            If you are looking for a programmer just write to me and I will gladly become a part of your team!
-                        </InformationAbout>
-                        <NavLink to='/contact'>
-                            <Button>Hire me</Button>
-                        </NavLink>
-                    </ProfileInformation>
-                </ProfileWrapper>
-            </Container>
-        </Router>
+        <Container>
+            <ProfileWrapper>
+                <ProfileImage>
+                    <ImagePerson src={Roman} alt="Roman" />
+                </ProfileImage>
+                <ProfileInformation>
+                    <InformationName>
+                        Roman Gulamov
+                        <InformationPosition>
+                            Front-end Developer
+                        </InformationPosition>
+                    </InformationName>
+                    <InformationAbout>
+                        <span role="img" aria-label="give-five">Hello! &#128588;</span> {'\n'}
+                        <span role="img" aria-label="give-five">
+                            I am Roman Gulamov, Front-end Developer from Saint Petersburg &#127969;
+                        </span> {'\n'}  
+                        If you are looking for a programmer just write to me and I will gladly become a part of your team!
+                    </InformationAbout>
+                    <Link to='/contact'>
+                        <Button>Hire me</Button>
+                    </Link>
+                </ProfileInformation>
+            </ProfileWrapper>
+        </Container>
         </>
     )
 }
