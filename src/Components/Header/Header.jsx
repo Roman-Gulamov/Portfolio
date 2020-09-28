e1434cc91a86013b9f46cd3b12e8adde957648ca
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-29 02:37:53 +0300

    Added display of a specific menu in which you are

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { HashRouter as Router } from 'react-router-dom';
 
 import { LinksMap } from './LinksMap';
 
@@ -10,39 +9,49 @@ import {
     Line,
     NavWrapper,
     NavInvisible,
-    NavLogo } from '../../styles/Header';
+    NavTitle } from '../../styles/Header';
+
 
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState('');
-    
+    const [title, setTitle] = useState(window.location.hash.replace('#/', ''));
+
     const toggleMenu = () => {
         if (window.innerWidth <= 768) {
             isOpen === 'open' ? setIsOpen('') : setIsOpen('open');
         }
-    }   
+    }
+
+    const handleTitle = () => {
+        setTitle(window.location.hash.replace('#/', ''))
+    }
+
+    window.onhashchange = () => handleTitle();
+
 
     return (
         <>
-        <Router>
-            <HeaderWrapper>
-                <HeaderNav>
-                    <NavHumburger 
-                        onClick={toggleMenu} 
-                        position={isOpen === 'open' ? 'fixed' : 'absolute'}
-                    >
-                        <Line />
-                        <Line />
-                        <Line />
-                    </NavHumburger>
-                    <NavLogo>Roman Gulamov</NavLogo>
-                    <NavWrapper className={isOpen}>
-                        <LinksMap onClick={toggleMenu} />
-                        <NavInvisible className={isOpen} onClick={toggleMenu}></NavInvisible>
-                    </NavWrapper>
-                </HeaderNav>
-            </HeaderWrapper>
-        </Router>
+        <HeaderWrapper>
+            <HeaderNav>
+                <NavHumburger 
+                    onClick={toggleMenu} 
+                    position={isOpen === 'open' ? 'fixed' : 'absolute'}
+                >
+                    <Line />
+                    <Line />
+                    <Line />
+                </NavHumburger>
+                <NavTitle>{title}</NavTitle>
+                <NavWrapper open={isOpen}>
+                    <LinksMap toggleMenu={toggleMenu} />
+                    <NavInvisible
+                        open={isOpen}
+                        onClick={toggleMenu}
+                    />
+                </NavWrapper>
+            </HeaderNav>
+        </HeaderWrapper>
         </>
     );
 }
