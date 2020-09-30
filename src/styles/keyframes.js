a39a7c716b960713f38fecf6e6b83422c0111bb5
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-30 20:55:04 +0300

    Keyframes have been moved to a separate file

---
+++ src/styles/keyframes.js
@@ -0,0 +1,70 @@
+import { keyframes } from 'styled-components';
+
+
+export const progress = (props) => { 
+    return keyframes`
+        from {
+            width: 10%;
+        }
+        to {
+            width: ${props.procent || '0%'};
+        }
+    `
+}
+
+
+export const rotate = () => { 
+    return keyframes`
+        100% {
+            transform: rotate(360deg);
+        }
+    `
+}
+
+export const stroke = (props) => { 
+    return keyframes`
+        ${props.procent || '0%'} {
+            stroke-dashoffset: 0;
+        }
+    `
+}
+
+export const scale = () => { 
+    return keyframes`
+        0%, 100% {
+            transform: none;
+        }
+        50% {
+            transform: scale3d(1.1, 1.1, 1);
+        }
+    `
+}
+
+export const fill = () => { 
+    return keyframes`
+        100% {
+            box-shadow: inset 0px 0px 0px 30px #4bb71b;
+        }
+    `
+}
+
+export const circle = () => { 
+    return keyframes`
+        0% {
+            stroke-dasharray: 0, 260.75219024795285px;
+            stroke-dashoffset: 0;
+        }
+        35% {
+            stroke-dasharray: 120px, 120px;
+            stroke-dashoffset: -120px;
+        }
+        70% {
+            stroke-dasharray: 0, 260.75219024795285px;
+            stroke-dashoffset: -260.75219024795285px;
+        }
+        100% {
+            stroke-dasharray: 260.75219024795285px, 0;
+            stroke-dashoffset: -260.75219024795285px;
+        }
+    `
+}
