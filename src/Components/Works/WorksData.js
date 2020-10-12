25226fc1c19fbf70b44dfb0e71d08fbcad3d6f19
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-12 06:03:22 +0300

    Added new works

--- src/Components/Works/WorksData.js
+++ src/Components/Works/WorksData.js
@@ -1,3 +1,4 @@
+import PhotoGallery from '../../assets/images/works/PhotoGallery.png';
 import TrainsAggregator from '../../assets/images/works/TrainsAggregator.png';
 import Rapture from '../../assets/images/works/Rapture.png';
 import Subjects from '../../assets/images/works/Subjects.png';
@@ -10,41 +11,48 @@ import Leo from '../../assets/images/works/Leo.png';
 export const WORKS_DATA = [
     {
         id: '1',
+        img: PhotoGallery,
+        git: 'https://github.com/Roman-Gulamov/Photogallery',
+        ghPages: 'https://roman-gulamov.github.io/Photogallery/#/users',
+        languageName: 'React/Styled-components'
+    },
+    {
+        id: '2',
         img: TrainsAggregator,
         git: 'https://github.com/Roman-Gulamov/train-search',
         ghPages: 'https://roman-gulamov.github.io/train-search',
         languageName: 'React/SCSS'
     },
     {
-        id: '2',
+        id: '3',
         img: Rapture,
         git: 'https://github.com/Roman-Gulamov/Rapture',
         ghPages: null,
         languageName: 'HTML/SCSS/jQuery/Gulp'
     },
     {
-        id: '3',
+        id: '4',
         img: Subjects,
         git: 'https://github.com/Roman-Gulamov/Subjects',
         ghPages: 'https://roman-gulamov.github.io/Subjects',
         languageName: 'React/SCSS'
     },
     {
-        id: '4',
+        id: '5',
         img: TypeScriptForm,
         git: 'https://github.com/Roman-Gulamov/axiosform',
         ghPages: null,
         languageName: 'TypeScript/React/PHP'
     },
     {
-        id: '5',
+        id: '6',
         img: TicTac,
         git: 'https://github.com/Roman-Gulamov/Tic-tac-toe',
         ghPages: 'https://roman-gulamov.github.io/Tic-tac-toe',
         languageName: 'React/SCSS'
     },
     {
-        id: '6',
+        id: '7',
         img: Leo,
         git: 'https://github.com/Roman-Gulamov/Leo',
         ghPages: null,
