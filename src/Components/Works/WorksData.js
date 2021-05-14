f998b01a794313eb3106f6e49988488f27c36ca9
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-14 22:53:17 +0300

    Updated Works data

--- src/Components/Works/WorksData.js
+++ src/Components/Works/WorksData.js
@@ -1,17 +1,17 @@
 import Startup from '../../assets/images/works/Startup.png';
-import TrainsAggregator from '../../assets/images/works/TrainsAggregator.png';
-import Rapture from '../../assets/images/works/Rapture.png';
-import Subjects from '../../assets/images/works/Subjects.png';
-import TypeScriptForm from '../../assets/images/works/TypeScriptForm.png';
-import TicTac from '../../assets/images/works/TicTac.png';
-import UserProfile from '../../assets/images/works/UserProfile.png';
-import Leo from '../../assets/images/works/Leo.png';
 import FavoriteUsers from '../../assets/images/works/FavoriteUsers.png';
-import LiveJournal from '../../assets/images/works/LiveJournal.png';
-import PhotoGallery from '../../assets/images/works/PhotoGallery.png';
+import Rapture from '../../assets/images/works/Rapture.png';
+import UserProfile from '../../assets/images/works/UserProfile.png';
 import SortableTable from '../../assets/images/works/SortableTable.png';
+import Launches from '../../assets/images/works/Launches.png';
+import TrainsAggregator from '../../assets/images/works/TrainsAggregator.png';
+import TypeScriptForm from '../../assets/images/works/TypeScriptForm.png';
 import BuhOne from '../../assets/images/works/BuhOne.png';
-
+import Leo from '../../assets/images/works/Leo.png';
+import PhotoGallery from '../../assets/images/works/PhotoGallery.png';
+import TicTac from '../../assets/images/works/TicTac.png';
+import Subjects from '../../assets/images/works/Subjects.png';
+import LiveJournal from '../../assets/images/works/LiveJournal.png';
 
 
 export const WORKS_DATA = [
@@ -52,55 +52,62 @@ export const WORKS_DATA = [
     },
     {
         id: '6',
+        img: Launches,
+        git: 'https://github.com/Roman-Gulamov/Launches',
+        ghPages: 'https://roman-gulamov.github.io/Launches',
+        languageName: 'TypeScript/React'
+    },
+    {
+        id: '7',
         img: TrainsAggregator,
         git: 'https://github.com/Roman-Gulamov/train-search',
         ghPages: 'https://roman-gulamov.github.io/train-search',
         languageName: 'React/SCSS'
     },
     {
-        id: '7',
+        id: '8',
         img: TypeScriptForm,
         git: 'https://github.com/Roman-Gulamov/axiosform',
         ghPages: null,
         languageName: 'TypeScript/React/PHP'
     },
     {
-        id: '8',
+        id: '9',
         img: BuhOne,
         git: 'https://github.com/Roman-Gulamov/Buhone',
         ghPages: 'https://roman-gulamov.github.io/Buhone',
         languageName: 'React/Styled-components'
     },
     {
-        id: '9',
+        id: '10',
         img: Leo,
         git: 'https://github.com/Roman-Gulamov/Leo',
         ghPages: null,
         languageName: 'HTML/SCSS/jQuery/Webpack'
     },
     {
-        id: '10',
+        id: '11',
         img: PhotoGallery,
         git: 'https://github.com/Roman-Gulamov/Photogallery',
         ghPages: 'https://roman-gulamov.github.io/Photogallery/#/users',
         languageName: 'React/Styled-components'
     },
     {
-        id: '11',
+        id: '12',
         img: TicTac,
         git: 'https://github.com/Roman-Gulamov/Tic-tac-toe',
         ghPages: 'https://roman-gulamov.github.io/Tic-tac-toe',
         languageName: 'React/SCSS'
     },
     {
-        id: '12',
+        id: '13',
         img: Subjects,
         git: 'https://github.com/Roman-Gulamov/Subjects',
         ghPages: 'https://roman-gulamov.github.io/Subjects',
         languageName: 'React/SCSS'
     },
     {
-        id: '13',
+        id: '14',
         img: LiveJournal,
         git: 'https://github.com/Roman-Gulamov/ListOfPosts',
         ghPages: 'https://roman-gulamov.github.io/ListOfPosts',
