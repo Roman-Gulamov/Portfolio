4bce969948d0633ea5ee4e23050dadf253276660
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-24 22:11:37 +0300

    Updated markup

--- src/index.jsx
+++ src/index.jsx
@@ -5,5 +5,6 @@ import { App } from './Components/App';
 import './assets/fonts/kufam/stylesheet.css';
 import './assets/fonts/architectsDaughter/stylesheet.css';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
